a6a2052156235f6bf57b9ed00f0e00ff8ac3938f
Jun-Araki <jun7772006@gmail.com>
2023-03-31 16:06:02 +0900

    回答No.11

--- task.rb
+++ task.rb
@@ -115,6 +115,11 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports2 = sports.flatten.uniq
+
+  sports2.each.with_index(1) do |name, i|
+    puts "No#{i} #{name}"
+  end
 end
 
 def q12
